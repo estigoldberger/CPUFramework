dd2ec5fe0578ded9ee77af93278fe08c9a494938
estigoldberger <estigabb@gmail.com>
2024-02-14 11:29:34 -0500

    execute sql

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -21,5 +21,9 @@ namespace CPUFramework
             dt.Load(dr);
             return dt;
         }
+        public static void ExecuteSQL(string sqlstatement)
+        {
+            GetDataTable(sqlstatement);
+        }
     }
 }
